56f504e5aea23511d2c3ce4ac1319a1fa94b3ad4
MUHAMMADMUKHTARSULEIMAN <muhammadmukhtarsuleiman@gmail.com>
2023-12-23 12:39:23 +0100

    just another commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useReducer} from "react";
+import Task from "./components/Task";
+
+const initialTasks = [];
+let setId = 0;
+
+function tasksReducer(state, action) {
+  switch(action.type) {
+    case "create": {
+      state.push({
+        id: action.id,
+        text: action.text,
+        done: false,
+        edit: false
+      });
+      break;
+    }
+    case "delete": {
+      return state.filter(t => t.id !== action.id);
+    }
+    case "done": {
+      const toggle = state.find(t => t.id === action.id);
+      toggle.done = !toggle.done;
+      break;
+    }
+    case "edit": {
+      const toggle = state.find(t => t.id === action.id);
+      toggle.edit = !toggle.edit;
+      break;
+    }
+    default: {
+      throw Error(`Unknown action: ${action.type}`)
+    }
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+  const toggleDone = taskId => {
+    dispatch({
+      type: "done",
+      id: taskId
+    });
+  };
+
+  const editTask = taskId => {
+    dispatch({
+      type: "edit",
+      id: taskId
+    });
+  };
+
+  const addTask = e => {
+    e.preventDefault();
+
+    dispatch({
+      type: "create",
+      id: setId++,
+      text: ""
+    });
+  };
+
+  const deleteTask = taskId => {
+    dispatch({
+      type: "delete",
+      id: taskId
+    });
+  };
+
+  const [form, setForm] = useState({
+    text: ""
+  });
+
+
+  const handleChange = e => {
+    const {name, value} = e.target;
+
+    setForm(prevForm => {
+      return {
+        ...prevForm,
+        [name]: value
+      }
+    })
+  }
+};
+
+
 
 export default App;
